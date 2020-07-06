4d0bad0667dca79bb8a7d9793b8a64025439999e
Raymond Albright <razer523@hotmail.com>
2020-07-06 14:38:12 -0700

    Finished CSS notes

--- css.js
+++ css.js
@@ -37,3 +37,43 @@ function pseudoclasses() {
 function fontfamily() {
     alert("p{ \n font-family: san-serif; \n } \n USE Google for more fonts (font names with spaces must be wrapped in ' ' \n IF OS does not have font, give multiple options: \n font-family: 'Trebuchet MS', arial, helvetica, sans-serif \n } \n The first has priority, OS will go to next if first is not available.")
 }
+
+function webfonts(){
+    alert("To use a font from a URL, you must first link font to the head above your CSS file. \n <head> \n <link href=\"FONT-URL\" rel=\"stylesheet\"> \n <link rel=\"stylesheet\" href=\"NAME.css\"> \n </head>")
+}
+
+function borders(){
+    alert("To add a border to a box of text:\n p{ \n border-style: solid; \n } \n solid, none, hidden, dotted, dashed, double, etc.... \n Multiple Borders= Top-Right-Bottom-Left \n sperate it by commas. ")
+}
+
+function borderformat(){
+    alert("To format your border, use: \n p { \n border-style: _______ \n border-width: _______ \n border-color: _______ \n OR \n p { \n border: WIDTH STYLE COLOR (no commas) \n }") 
+}
+
+function positioning(){
+    alert("To change the position of a text area, use: position: _____ \n nav { \n position: absolute; \n } \n Absolute: locks line to screen outside of content. \n Fixed: locks line to window. \n Static: DEFAULT, in line with text. \n Relative: keeps static look but moves area to given location using top: left: info.")
+}
+
+function boxmodel(){
+    alert("The CSS box model is essentially a box that wraps around every HTML element. It consists of: margins, borders, padding, and the actual content.")
+}
+
+function margin(){
+    alert("The margin is the area around the elements, outside of the border. Does not change the size of the Element. \n p { \n margin-top: 20px; \n margin-right: 20px; \n margin-bottom: 20px; \n margin-left: 20px; \n } \n Defined in pixels (px)")
+}
+
+function border(){
+    alert("Second layer in the Box model, border that goes aorund the content. \ndotted - Defines a dotted border \ndashed - Defines a dashed border \nsolid - Defines a solid border \ndouble - Defines a double border \ngroove - Defines a 3D grooved border. \n*The effect depends on the border-color value \nridge - Defines a 3D ridged border. \n*The effect depends on the border-color value \ninset - Defines a 3D inset border. \n*The effect depends on the border-color value \noutset - Defines a 3D outset border. \n*The effect depends on the border-color value\nnone - Defines no border\nhidden - Defines a hidden border ")
+}
+
+function padding(){
+    alert("The padding is the space between the border and the content. \nValues to use for defining: \npadding-top: 20px; \npadding-right: 20px; \npadding-bottom: 20px; \npadding-left: 20px;")
+}
+
+function content(){
+    alert("The content of the box, where text and images appear. Use width and height to adjust the content area only. \nwidth: 20px; \nheight: 20px;")
+}
+
+function float(){
+    alert("The float property is used for positioning and formatting content e.g. let an image float left to the text in a container. This prevents it from only being displayed on a single line of text.\n\n  left - The element floats to the left of its container\nright - The element floats to the right of its container\nnone - The element does not float (will be displayed just where it occurs in the text). This is default\ninherit - The element inherits the float value of its parent\n\nIn its simplest use, the float property can be used to wrap text around images.")
+}
